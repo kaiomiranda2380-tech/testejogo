a0671c74bee9fa140f249afedbeabc6a836fd1af
kaiomiranda2380-tech <kaiomiranda2380@gmail.com>
2026-09-14 08:43:09 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,46 +1,33 @@
-let jogador;
-let teclas;
-
 const configuracao = {
     type: Phaser.AUTO,
-
     width: 800,
     height: 600,
-
     parent: "jogo",
-
     backgroundColor: "rgb(32, 32, 32)",
-
     physics: {
         default: "arcade",
-        arcade: {
-            debug: false
-        }
+        arcade: { debug: false }
     },
-
     scene: {
-        preload: preload,
-        create: create,
-        update: update
+        preload,
+        create,
+        update
     }
 };
 
-const jogo = new Phaser.Game(configuracao);
+new Phaser.Game(configuracao);
 
 function preload() {
-
     this.load.image("personagem", "img/personagem.png");
 }
 
 function create() {
+    
+    this.jogador = this.physics.add.image(100, 300, "personagem");
+    this.jogador.setDisplaySize(70, 70);
+    this.jogador.setCollideWorldBounds(true);
 
-    jogador = this.physics.add.image(100, 300, "personagem");
-
-    jogador.setDisplaySize(70, 70);
-
-    jogador.setCollideWorldBounds(true);
-
-    teclas = this.input.keyboard.createCursorKeys();
+    this.teclas = this.input.keyboard.createCursorKeys();
 
     this.add.text(20, 20, "Use as setas para movimentar", {
         fontSize: "24px",
@@ -49,23 +36,20 @@ function create() {
 }
 
 function update() {
+    const jogador = this.jogador;
+    const teclas = this.teclas;
+    const velocidade = 200;
 
     jogador.setVelocity(0);
 
-    if (teclas.left.isDown) {
-        jogador.setVelocityX(-200);
-    }
+    if (teclas.left.isDown) jogador.setVelocityX(-velocidade);
+    else if (teclas.right.isDown) jogador.setVelocityX(velocidade);
 
-    if (teclas.right.isDown) {
-        jogador.setVelocityX(200);
-    }
+    if (teclas.up.isDown) jogador.setVelocityY(-velocidade);
+    else if (teclas.down.isDown) jogador.setVelocityY(velocidade);
 
-    if (teclas.up.isDown) {
-        jogador.setVelocityY(-200);
-    }
-
-    if (teclas.down.isDown) {
-        jogador.setVelocityY(200);
+    if (jogador.body.velocity.x !== 0 && jogador.body.velocity.y !== 0) {
+        jogador.body.velocity.normalize().scale(velocidade);
     }
 }
 
